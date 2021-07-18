b177a8ecf1fd588c485c755602003c65d1e1af3f
cameronmcnz <you@example.com>
2021-07-18 23:16:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cart from './Cart';
+import Badge from './Badge';
+
+const App = () => {
+  return (
+    <div>
+        <Badge />
+        <Cart />
+    </div>
+  )
+}
+
+export default App
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import {createStore,applyMiddleware} from 'redux';
+import reducer from './reducer';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+const middleware = [thunk]
+const store =createStore(reducer,composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
